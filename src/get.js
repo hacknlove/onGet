e734df6b7829c47bd7f98be6c69e6911a59a846a
Antonio Fernández Porrúa <antonio@pykiss.com>
2019-09-19 08:35:38 +0200

    document

--- src/get.js
+++ src/get.js
@@ -1,11 +1,20 @@
 import { findPlugin } from './findPlugin'
 import { endpoints } from './conf'
 
-export function get (url) {
+/**
+ * Returns the cached value for the endpoint
+ * @param {string} url url of the endpoint
+ * @param {boolean} onlyCached=true, set to false to force the plugin to obtain a value if none if cached
+ * @returns {any} whatever value is cached, or undefined, (or the obtained value if onlyCached = false)
+ */
+export function get (url, onlyCached = true) {
   const endpoint = endpoints[url]
   if (endpoint) {
     endpoint.clean = undefined
     return endpoints[url].value
+  }
+  if (onlyCached) {
+    return undefined
   }
   const plugin = findPlugin(url)
   if (!plugin.get) {
