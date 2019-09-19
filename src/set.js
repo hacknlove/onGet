ac5aaa94224a218f6c36b74c86a437c44aa26f4b
Antonio Fernández Porrúa <antonio@pykiss.com>
2019-09-19 08:34:49 +0200

    fix and document

--- src/set.js
+++ src/set.js
@@ -2,10 +2,22 @@ import isDifferent from 'isdifferent'
 import { endpoints } from './conf'
 import { getEndpoint } from './getEndpoint'
 
+/**
+ * set a new value for an endpoint, and call the handlers. If the endpoint does not exists, it creates it.
+ * @param {string} url  of the endpoint whose value set to
+ * @param {any} value value to series
+ * @param {boolean} pospone=true if false do not pospone the closest refresh
+ * @return {object} endpoint
+ */
 export function set (url, value, pospone) {
-  var notNew = endpoints.hasOwnProperty(url)
+  var isNew = !endpoints.hasOwnProperty(url)
 
   const endpoint = getEndpoint(url, value)
+
+  if (isNew) {
+    return endpoint
+  }
+
   endpoint.clean = undefined
 
   if (endpoint.intervals) {
@@ -19,4 +31,6 @@ export function set (url, value, pospone) {
     endpoint.value = value
     Object.values(endpoint.callbacks).forEach(cb => cb(endpoint.value))
   }
+
+  return endpoint
 }
