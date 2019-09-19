75369cb6d0004517e60c24907ff99ef9f29a04a9
Antonio Fernández Porrúa <antonio@pykiss.com>
2019-09-19 08:35:24 +0200

    document

--- src/onGet.js
+++ src/onGet.js
@@ -2,6 +2,13 @@ import { getEndpoint } from './getEndpoint'
 import { addNewSuscription } from './addNewSuscription'
 import { refresh } from './refresh'
 
+/**
+ * Set a handler to be called each time the value of the url changes
+ * @param {string} url The value to suscribe to
+ * @param {function} cb handler to be called
+ * @param {integer} interval seconds to refresh the value
+ * @param {any} first first value to pass to the plugin
+ */
 export function onGet (url, cb, interval, first) {
   const endpoint = getEndpoint(url, first)
   const unsuscribe = addNewSuscription(url, cb, interval)
