c85c5ae8b2aa15faff22ad8b21f9eaae8c9785de
antonio <antonio@pykiss.com>
2019-09-25 13:55:57 +0200

    preparing build

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -2,12 +2,11 @@
 
 const plugin = {
   name: 'fetch',
-  fetch: fetch,
   regex: /^./,
   checkInterval: 30,
   threshold: 500,
   async refresh (endpoint, eventHandler) {
-    const response = await plugin.fetch(endpoint.url).catch(error => ({ error }))
+    const response = await fetch(endpoint.url).catch(error => ({ error }))
     if (response.error) {
       return eventHandler(response.error)
     }
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+var config = {
+  mode: process.env.MODE,
+  entry: path.resolve(__dirname, 'src/index.js'),
+  plugins: [],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'onGet.js',
+    library: 'onGet',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'this'
+  },
+  externals: {
+    '@hacknlove/deepobject': {
+      commonjs: '@hacknlove/deepobject',
+      commonjs2: '@hacknlove/deepobject',
+      amd: '@hacknlove/deepobject',
+      root: 'deepObject'
+    },
+    isdifferent: {
+      commonjs: 'isdifferent',
+      commonjs2: 'isdifferent',
+      amd: 'isdifferent',
+      root: 'isDifferent'
+    }
+  }
+}
+
+if (config.mode === 'production') {
+  config.optimization = {
+    minimize: true
+  }
+  config.output.filename = 'onGet.min.js'
+} else {
+  config.devtool = 'source-map'
+}
+
+module.exports = config
