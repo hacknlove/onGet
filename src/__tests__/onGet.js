8a4631686273551a07fda4c15c8620fd2ba40340
Antonio Fernández Porrúa <antonio@pykiss.com>
2019-09-26 10:22:11 +0200

    100% coverage

--- src/__tests__/onGet.js
+++ src/__tests__/onGet.js
@@ -2,10 +2,11 @@ import { getEndpoint } from '../getEndpoint'
 import { addNewSubscription } from '../addNewSubscription'
 import { endpoints } from '../conf'
 import { onGet } from '../onGet'
+import { refresh } from '../refresh'
 
 jest.mock('../getEndpoint')
 jest.mock('../addNewSubscription')
-
+jest.mock('../refresh')
 beforeEach(() => {
   Object.keys(endpoints).forEach(key => delete endpoints[key])
 })
@@ -50,4 +51,32 @@ test('calls callback if endpoint.value != undefined', () => {
   onGet('test', callback)
 
   expect(callback).toHaveBeenCalledWith('ok testing')
+})
+
+test('calls refresh if it is called outside the threshold', () => {
+  getEndpoint.mockReturnValue({
+    plugin: {
+      threshold: 1000
+    },
+    value: 'ok testing',
+    last: Date.now() - 10000
+  })
+  const callback = jest.fn().mockReturnValue('ok testing')
+  onGet('test url', callback)
+
+  expect(refresh).toHaveBeenCalledWith('test url')
+})
+
+test('does not call refresh if it is called inside the threshold', () => {
+  getEndpoint.mockReturnValue({
+    plugin: {
+      threshold: 1000
+    },
+    value: 'ok testing',
+    last: Date.now() - 100
+  })
+  const callback = jest.fn().mockReturnValue('ok testing')
+  onGet('test url', callback)
+
+  expect(refresh).not.toHaveBeenCalledWith('test url')
 })
