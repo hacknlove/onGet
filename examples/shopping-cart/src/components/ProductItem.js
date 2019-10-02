458dd93de5cf222f684f242f8ef4bc1160419112
antonio <antonio@pykiss.com>
2019-10-02 15:50:15 +0200

    finished

--- examples/shopping-cart/src/components/ProductItem.js
+++ examples/shopping-cart/src/components/ProductItem.js
@@ -1,17 +1,17 @@
 /* global fetch */
 import React from 'react'
 import Product from './Product'
-import { refresh,get, set } from 'onget'
+import { refresh, get, set } from 'onget'
 
 async function addToCart (product) {
   await fetch(`/api/cart/${product.id}`, {
     method: 'post'
   })
 
-  // we know that the value at /api/cart has changed so we call refresh to avoid waiting for the periodicall check
-  refresh('/api/cart')
+  // ask a forced refresh for '/api/cart'
+  refresh('/api/cart', true)
 
-  // we know that the inventory should have been reduced by one, so we can optimistically set the new expected value at /api/product
+  // guess optimistically the new value at '/api/products'
   const products = [...get('/api/products')]
   products[product.id] = {
     ...products[product.id],
