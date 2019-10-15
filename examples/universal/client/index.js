57238486514af63e6f5c0f99c8ddc2aa1f0730b6
antonio <antonio@pykiss.com>
2019-10-15 23:03:05 +0200

    sync

--- examples/universal/client/index.js
+++ examples/universal/client/index.js
@@ -2,15 +2,15 @@ import '@babel/polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import App from '../common/components/App'
-import { beforeSet, load } from 'onget'
+import { beforeSet, load, get } from 'onget'
 
 const rootElement = document.getElementById('app')
 
 load(__PRELOADED_STATE__)
 
 beforeSet('/api/v1/counter', event => {
-  fetch()
-  console.log(end)
+  // fetch()
+  console.log(get('/api/v1/counter'))
   console.log(event)
 })
 
